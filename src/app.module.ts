620e1d4700447e9f794d0912fabe39cb11089d07
Benjamin Niddam <nddm.benjamin@gmail.com>
2022-08-18 20:47:00 +0200

    Adding TypeORM Module for postgresql connexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-52-30-75-37.eu-west-1.compute.amazonaws.com',
+      database: 'dc9n4oaah79rir',
+      username: 'bvmacnaytiesct',
+      port: 5432,
+      password:
+        '2a0ad6303a39580c7e44b796a2a933a4c495d310b7264f10a6df4cfae6ab35d9',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
