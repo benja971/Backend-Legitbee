e4f0b4493ee61d1c85d209a379f5d48dec93e46c
Benjamin Niddam <nddm.benjamin@gmail.com>
2022-08-21 00:25:18 +0200

    Cars routes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarInput } from './dto/create-car.input';
+import { UpdateCarInput } from './dto/update-car.input';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
+  @Post()
+  create(@Body() createCarInput: CreateCarInput) {
+    return this.carsService.create(createCarInput);
+  }
+
   @Get()
   findAll() {
     return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.carsService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateCarInput: UpdateCarInput) {
+    return this.carsService.update(id, updateCarInput);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.carsService.remove(id);
   }
 }
